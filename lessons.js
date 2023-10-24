f563e8d77a47c8807f766b52198bcbee2fcc123c
zyadexe <lyzyad53@gmail.com>
2023-10-24 22:36:34 +0300

    hello :)

---
+++ lessons.js
@@ -0,0 +1,28 @@
+let lessonProgress = parseFloat(localStorage.getItem('lessonProgress')) || 0;
+
+
+    updateProgressBar();
+let btn =null;
+function completeLesson(button) {
+    lessonProgress += 11.11; // Increase the progress by 11.11% for each lesson
+    button.disabled = true; // Disable the button after marking as complete
+    btn=button;
+    updateProgressBar();
+    // Store the updated progress in local storage
+    localStorage.setItem('lessonProgress', lessonProgress);
+}
+
+function resetProgress() {
+    lessonProgress = 0; // Reset the progress to 0
+    localStorage.setItem('lessonProgress', lessonProgress); // Update the local storage
+    if (btn !== null) {
+        btn.disabled=false;
+    }
+    updateProgressBar();
+}
+
+
+function updateProgressBar() {
+    const progressBar = document.getElementById('lesson-progress');
+    progressBar.value = lessonProgress;
+}
