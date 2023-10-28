38ba2787e8c74c78624efc426f1689a412e30562
zyadexe <lyzyad53@gmail.com>
2023-10-28 18:12:13 +0300

    format lesson.js

--- lessons.js
+++ lessons.js
@@ -1,28 +1,26 @@
-let lessonProgress = parseFloat(localStorage.getItem('lessonProgress')) || 0;
+let lessonProgress = parseFloat(localStorage.getItem("lessonProgress")) || 0;
 
-
-    updateProgressBar();
-let btn =null;
+updateProgressBar();
+let btn = null;
 function completeLesson(button) {
-    lessonProgress += 11.11; // Increase the progress by 11.11% for each lesson
-    button.disabled = true; // Disable the button after marking as complete
-    btn=button;
-    updateProgressBar();
-    // Store the updated progress in local storage
-    localStorage.setItem('lessonProgress', lessonProgress);
+  lessonProgress += 11.11; // Increase the progress by 11.11% for each lesson
+  button.disabled = true; // Disable the button after marking as complete
+  btn = button;
+  updateProgressBar();
+  // Store the updated progress in local storage
+  localStorage.setItem("lessonProgress", lessonProgress);
 }
 
 function resetProgress() {
-    lessonProgress = 0; // Reset the progress to 0
-    localStorage.setItem('lessonProgress', lessonProgress); // Update the local storage
-    if (btn !== null) {
-        btn.disabled=false;
-    }
-    updateProgressBar();
+  lessonProgress = 0; // Reset the progress to 0
+  localStorage.setItem("lessonProgress", lessonProgress); // Update the local storage
+  if (btn !== null) {
+    btn.disabled = false;
+  }
+  updateProgressBar();
 }
 
-
 function updateProgressBar() {
-    const progressBar = document.getElementById('lesson-progress');
-    progressBar.value = lessonProgress;
+  const progressBar = document.getElementById("lesson-progress");
+  progressBar.value = lessonProgress;
 }
